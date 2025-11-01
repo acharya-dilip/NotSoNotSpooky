6c160c7d23e52e5cbb9b121134937ff39049c77a
JCT-Biura <88521658+JCT-B@users.noreply.github.com>
2025-11-01 10:19:22 +0545

    init of entrycal and some changes to the CSS of entry and buttons

--- main.c
+++ main.c
@@ -23,52 +23,56 @@ static void activate (GtkApplication *app,gpointer user_data) {
     //Init of windowSpooky
     GtkWidget *windowSpooky = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(windowSpooky),"Calculator");
-    gtk_window_set_default_size(GTK_WINDOW(windowSpooky),600,600);
+    //gtk_window_set_default_size(GTK_WINDOW(windowSpooky),600,600);
     gtk_window_present(GTK_WINDOW(windowSpooky));
 
     //init of gridParent
     GtkWidget *gridParent = gtk_grid_new();
     gtk_window_set_child(GTK_WINDOW(windowSpooky),gridParent);
 
+    //init of entryCal
+    GtkWidget *entryCal = gtk_entry_new();
+    gtk_grid_attach(GTK_GRID(gridParent),entryCal,0,0,4,1);
+
     //init of numbut7
     GtkWidget *numbut7 = gtk_button_new_with_label("7");
-    gtk_grid_attach(GTK_GRID(gridParent),numbut7,0,1,1,1);
+    gtk_grid_attach(GTK_GRID(gridParent),numbut7,0,2,1,1);
 
     //init of numbut8
     GtkWidget *numbut8 = gtk_button_new_with_label("8");
-    gtk_grid_attach(GTK_GRID(gridParent),numbut8,1,1,1,1);
+    gtk_grid_attach(GTK_GRID(gridParent),numbut8,1,2,1,1);
 
     //init of numbut9
     GtkWidget *numbut9 = gtk_button_new_with_label("9");
-    gtk_grid_attach(GTK_GRID(gridParent),numbut9,2,1,1,1);
+    gtk_grid_attach(GTK_GRID(gridParent),numbut9,2,2,1,1);
 
     //init of numbut6
     GtkWidget *numbut6 = gtk_button_new_with_label("6");
-    gtk_grid_attach(GTK_GRID(gridParent),numbut6,0,2,1,1);
+    gtk_grid_attach(GTK_GRID(gridParent),numbut6,0,3,1,1);
 
     //init of numbut5
     GtkWidget *numbut5 = gtk_button_new_with_label("5");
-    gtk_grid_attach(GTK_GRID(gridParent),numbut5,1,2,1,1);
+    gtk_grid_attach(GTK_GRID(gridParent),numbut5,1,3,1,1);
 
     //init of numbut4
     GtkWidget *numbut4 = gtk_button_new_with_label("4");
-    gtk_grid_attach(GTK_GRID(gridParent),numbut4,2,2,1,1);
+    gtk_grid_attach(GTK_GRID(gridParent),numbut4,2,3,1,1);
 
     //init of numbut1
     GtkWidget *numbut1 = gtk_button_new_with_label("1");
-    gtk_grid_attach(GTK_GRID(gridParent),numbut1,0,3,1,1);
+    gtk_grid_attach(GTK_GRID(gridParent),numbut1,0,4,1,1);
 
     //init of numbut2
     GtkWidget *numbut2 = gtk_button_new_with_label("2");
-    gtk_grid_attach(GTK_GRID(gridParent),numbut2,1,3,1,1);
+    gtk_grid_attach(GTK_GRID(gridParent),numbut2,1,4,1,1);
 
     //init of numbut3
     GtkWidget *numbut3 = gtk_button_new_with_label("3");
-    gtk_grid_attach(GTK_GRID(gridParent),numbut3,2,3,1,1);
+    gtk_grid_attach(GTK_GRID(gridParent),numbut3,2,4,1,1);
 
     //init of numbut0
     GtkWidget *numbut0 = gtk_button_new_with_label("0");
-    gtk_grid_attach(GTK_GRID(gridParent),numbut0,1,4,1,1);
+    gtk_grid_attach(GTK_GRID(gridParent),numbut0,1,5,1,1);
 
 
 }
